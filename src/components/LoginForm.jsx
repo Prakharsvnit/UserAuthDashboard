1b2fdfcef1af37a1944341140272d4a6505ed11c
Prakharsvnit <tprakhar468@gmail.com>
2022-12-12 20:25:06 +0530

    first commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,19 @@
+import { Box, Button, TextField, Typography,Container } from "@mui/material";
+
+export default function LoginForm() {
+  return (
+    <>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          flexDirection: "column",
+          display: "flex",
+          height:600,
+          mt:15
+        }}
+      >
+      </Box>
+      </Container>
+    </>
+  );
+}
